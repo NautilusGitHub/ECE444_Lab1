29f466868ebd70475679a5bf857ab893adc9c237
Zeryllium <davidcw.zhang@mail.utoronto.ca>
2020-09-16 10:53:46 -0400

    Refactored print statement into its own method

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,6 @@
+def main():
+    print("Hello World! My name is Jason Shang. I've been at Uoft for 4 years.")
+
+
 if __name__ == "__main__":
-    print("Hello World! My name is Jason Shang. I've been at Uoft for 4 year")
+    main()
